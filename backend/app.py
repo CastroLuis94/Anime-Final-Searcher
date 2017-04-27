4a1a48a8035c30c0df4ad59a055030220c9880c9
David Litvak Bruno <david.litvakb@gmail.com>
2017-04-27 17:30:31 -0300

    Add CORS

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, jsonify
 from sources import animes
+from crossdomain import crossdomain
+
 
 app = Flask(__name__)
 
 @app.route('/')
+@crossdomain(origin='*')
 def root():
     return jsonify({
         'status': 200,
